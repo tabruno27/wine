906e2454132c982a34d7e4db7001b43badaa7c90
tabruno27 <114284727+tabruno27@users.noreply.github.com>
2025-02-20 16:21:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,6 @@ from collections import defaultdict, OrderedDict
 CREAT_YEAR = 1920
 delta = datetime.now().year - CREAT_YEAR
 
-parser = argparse.ArgumentParser(description='Обработка данных из Excel.')
-parser.add_argument('--data_path', type=str, default='Production.xlsx', help='Путь к файлу с данными')
-
-args = parser.parse_args()
-
-excel_data_df = pd.read_excel(args.data_path, sheet_name='Лист1', na_values=['N/A', 'NA'], keep_default_na=False)
-products = excel_data_df.to_dict(orient='records')
-
-
 def get_year_word(n):
     if 11 <= n % 100 <= 14:
         return "лет"
@@ -28,36 +19,48 @@ def get_year_word(n):
         return "лет"
 
 
-year_word = get_year_word(delta)
+def main():
+    parser = argparse.ArgumentParser(description='Обработка данных из Excel.')
+    parser.add_argument('--data_path', type=str, default='Production.xlsx', help='Путь к файлу с данными')
 
-new_products = defaultdict(list)
+    args = parser.parse_args()
 
-for product in products:
-    category = product["Категория"]
-    wine = {
-        "Картинка": product["Картинка"],
-        "Категория": product["Категория"],
-        "Название": product["Название"],
-        "Сорт": product["Сорт"] if pd.notna(product["Сорт"]) else "",
-        "Цена": product["Цена"],
-        "Акция": product["Акция"] if pd.notna(product["Акция"]) else ""
-    }
-    new_products[category].append(wine)
+    excel_data_df = pd.read_excel(args.data_path, sheet_name='Лист1', na_values=['N/A', 'NA'], keep_default_na=False)
+    products = excel_data_df.to_dict(orient='records')
+
+    year_word = get_year_word(delta)
+
+    new_products = defaultdict(list)
+
+    for product in products:
+        category = product["Категория"]
+        wine = {
+            "Картинка": product["Картинка"],
+            "Категория": product["Категория"],
+            "Название": product["Название"],
+            "Сорт": product["Сорт"] if pd.notna(product["Сорт"]) else "",
+            "Цена": product["Цена"],
+            "Акция": product["Акция"] if pd.notna(product["Акция"]) else ""
+        }
+        new_products[category].append(wine)
     
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    
+    template = env.get_template('template.html')
+    
+    rendered_page = template.render(
+        cap_title=f"Уже {delta} {year_word} с вами",
+        new_products=new_products,
+    )
+    
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+    
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
 
-template = env.get_template('template.html')
-
-rendered_page = template.render(
-    cap_title=f"Уже {delta} {year_word} с вами",
-    new_products=new_products,
-)
-
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
-
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+if __main__=="__main__"
+    main()
